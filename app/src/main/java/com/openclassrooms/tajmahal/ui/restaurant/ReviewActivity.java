05578bab894f148b5ce25a5793df8a72c791432a
Afrogwada <afrogwadaweb@gmail.com>
2025-05-27 09:25:13 +0200

    Ajout nouvel écran ReviewActivity

---
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/ReviewActivity.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.tajmahal.ui.restaurant;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.openclassrooms.tajmahal.R;
+
+public class ReviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review); // Tu devras créer ce layout
+    }
+}
