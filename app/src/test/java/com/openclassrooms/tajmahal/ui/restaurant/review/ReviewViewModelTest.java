878b6c8173592bbe43a2a9c49c8981a60840388d
Afrogwada <afrogwadaweb@gmail.com>
2025-06-20 14:56:43 +0200

    modif tests AddReview et getReview

--- app/src/test/java/com/openclassrooms/tajmahal/ui/restaurant/review/ReviewViewModelTest.java
+++ app/src/test/java/com/openclassrooms/tajmahal/ui/restaurant/review/ReviewViewModelTest.java
@@ -1,8 +1,8 @@
 package com.openclassrooms.tajmahal.ui.restaurant.review;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.LiveData;
@@ -11,6 +11,7 @@ import androidx.lifecycle.MutableLiveData;
 import com.openclassrooms.tajmahal.data.repository.RestaurantRepository;
 import com.openclassrooms.tajmahal.domain.model.Review;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,23 +27,31 @@ import java.util.List;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ReviewViewModelTest {
+
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
     @Mock
     RestaurantRepository restaurantRepository;
 
     @InjectMocks
     ReviewViewModel reviewViewModel;
 
-    @Test
+    @Before
+    public void setUp() {
+        reviewViewModel = new ReviewViewModel(restaurantRepository);
+    }
+
+    @Test //test que le repository est appelé avec un review valide.
     public void addReview_shouldCallRepositoryAddReview() {
-        Review review = new Review("Alice", "", "Très bon restaurant",4);
+        Review review = new Review("Alice", "", "Très bon restaurant", 4);
 
         reviewViewModel.addReview(review);
 
         verify(restaurantRepository).addReview(review);
     }
-    @Test
+
+    @Test //test que si le repository lance une exception, la méthode la gère sans la propager.
     public void getReviews_shouldReturnLiveDataFromRepository() {
         List<Review> fakeReviews = Arrays.asList(
                 new Review("Alice", "photo1", "Commentaire 1", 5),
@@ -59,3 +68,4 @@ public class ReviewViewModelTest {
         assertEquals(fakeReviews, result.getValue());
     }
 }
+
